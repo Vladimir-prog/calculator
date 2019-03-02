f02e8f2af55c25e433f1ae315abec4ce587f9635
Vladimir Nastoyaschiy <maga_net@mail.ru>
2019-03-02 22:42:38 +0300

    Подсчет сложного выражения со скобками

--- src/com/company/Calculator.java
+++ src/com/company/Calculator.java
@@ -3,6 +3,7 @@ package com.company;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -18,59 +19,135 @@ public class Calculator {
         return parseIn;
     }
 
-    public static List<String> countExpression(List<String> input, int start, int finish){
+    public static Double countExpression(List<String> input) {
+        return Double.parseDouble(countExpression(input, 0, input.size() - 1));
+    }
+
+    private static String countExpression(List<String> input, int start, int finish) {
         List<String> helper = new ArrayList<>(input);
-        Double result = 0.0;
-        int elemIndex = 0;
-        int highPriorityAmount = howManyHighPriority(input);
-        System.out.println(highPriorityAmount);
-        int lowPriorityAmount = howManyLowPriority(input);
-        System.out.println(lowPriorityAmount);
-        int step = 1;
- //       while (highPriorityAmount > 0 ){
-            for (int i = 0; i < helper.size(); i++) {
-                if(helper.get(i).equals("*")||helper.get(i).equals("/")){
-                    input = countSimpleExpression(input, i-step);
-                    step = step + 2;
+        String result = "";
+        rightBracket:
+        for (int i = finish; i > start; i--) {
+            if (helper.get(i).equals(")")) {
+                int temp = 0;
+                for (int j = i - 1; j >= start; j--) {
+                    if ((helper.get(j).equals(")"))) {
+                        temp++;
+                    } else if (helper.get(j).equals("(") && temp != 0) {
+                        temp--;
+                    } else if (helper.get(j).equals("(")) {
+//                        brackets--;
+                        if (i == j + 2) {
+                            result = helper.get(j + 1);
+                            input = removeFromTo(input, j, i);
+                            input.add(j, result);
+                            i-=2;
+                        } else {
+                            result = countExpression(copyFromTo(input, j + 1, i - 1)).toString();
+                            input = removeFromTo(input, j, i);
+                            input.add(j, result);
+                            i-=i-j;
+                        }
+                    }
                 }
             }
-            helper = new ArrayList<>(input);
-            step = 1;
-  //          highPriorityAmount--;
- //       }
- //       while (lowPriorityAmount > 0 ){
+        }
+        result = countExpressionWithoutBrackets(input);
+        return result;
+    }
+
+    public static List<String> removeFromTo(List<String> input, int from, int to) {
+        //if (input.size() >= to) {
+            List<String> output = new LinkedList<>(input);
+            ListIterator listIterator = output.listIterator(from);
+            for (int i = from; i <= to; i++) {
+                listIterator.next();
+                listIterator.remove();
+            }
+            return output;
+//        } else return input;
+    }
+
+    public static List<String> copyFromTo(List<String> input, int from, int to) {
+        //if (input.size() >= to) {
+            ListIterator listIterator = input.listIterator(from);
+            List<String> output = new LinkedList<>();
+            for (int i = from; i <= to; i++) {
+                output.add(listIterator.next().toString());
+            }
+            return output;
+//        } else {
+//            return input;
+//        }
+    }
+
+    public static String countExpressionWithoutBrackets(List<String> input) {
+        List<String> helper = new ArrayList<>(input);
+        String result = "";
+//        int elemIndex = 0;
+//        int highPriorityAmount = howManyHighPriority(input);
+//        System.out.println(highPriorityAmount);
+//        int lowPriorityAmount = howManyLowPriority(input);
+//        System.out.println(lowPriorityAmount);
+        int step = 1;
+        //       while (highPriorityAmount > 0 ){
         for (int i = 0; i < helper.size(); i++) {
-            if(helper.get(i).equals("+")||helper.get(i).equals("-")){
-                input = countSimpleExpression(input, i-step);
+            if (helper.get(i).equals("*") || helper.get(i).equals("/")) {
+                result = countSimpleExpression(input, i - step);
+                input = removeFromTo(input, i - step, i - step + 2);
+                input.add(i - step, result);
                 step = step + 2;
             }
         }
- //           lowPriorityAmount--;
- //       }
-        return input;
+        helper = new ArrayList<>(input);
+        step = 1;
+        //          highPriorityAmount--;
+        //       }
+        //       while (lowPriorityAmount > 0 ){
+        for (int i = 0; i < helper.size(); i++) {
+            if (helper.get(i).equals("+") || helper.get(i).equals("-")) {
+                result = countSimpleExpression(input, i - step);
+                input = removeFromTo(input, i - step, i - step + 2);
+                input.add(i - step, result);
+                step = step + 2;
+            }
+        }
+        //           lowPriorityAmount--;
+        //       }
+        return input.get(0);
     }
 
-    public static int howManyHighPriority(List<String> input){
+    public static int howManyHighPriority(List<String> input) {
         int highPriorityAmount = 0;
         for (String elem : input) {
-            if (elem.equals("*")||elem.equals("/")){
+            if (elem.equals("*") || elem.equals("/")) {
                 highPriorityAmount++;
             }
         }
         return highPriorityAmount;
     }
 
-    public static int howManyLowPriority(List<String> input){
+    public static int howManyLowPriority(List<String> input) {
         int lowPriorityAmount = 0;
         for (String elem : input) {
-            if (elem.equals("+")||elem.equals("-")){
+            if (elem.equals("+") || elem.equals("-")) {
                 lowPriorityAmount++;
             }
         }
         return lowPriorityAmount;
     }
 
-    public static List<String> countSimpleExpression(List<String> input, int start) {
+    public static int howManyBrackets(List<String> input) {
+        int bracketsAmount = 0;
+        for (String elem : input) {
+            if (elem.equals("(")) {
+                bracketsAmount++;
+            }
+        }
+        return bracketsAmount;
+    }
+
+    public static String countSimpleExpression(List<String> input, int start) {
         List<String> output = new LinkedList<>(input);
         Double result = 0.0;
         switch (input.get(start + 1)) {
@@ -87,10 +164,10 @@ public class Calculator {
                 result = Double.parseDouble(input.get(start)) / Double.parseDouble(input.get(start + 2));
                 break;
         }
-        output.remove(start);
-        output.remove(start);
-        output.set(start, result.toString());
-        return output;
+//        output.remove(start);
+//        output.remove(start);
+//        output.set(start, result.toString());
+        return result.toString();
     }
 
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,7 @@
 package com.company;
 
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Main {
 
@@ -12,9 +9,9 @@ public class Main {
         InOutHelper.writeMessage("Привет! Введи выражение");
         String text = InOutHelper.readString();
         List<String> input = Calculator.parseInput(text);
-        InOutHelper.writeMessage(input.toString());
-        input = Calculator.countExpression(input, 0, input.size()-1);
-        InOutHelper.writeMessage(input.toString());
+//        InOutHelper.writeMessage(input.toString());
+        Double answer = Calculator.countExpression(input);
+        InOutHelper.writeMessage("ответ: " + answer);
 
 //        InOutHelper.writeMessage(Arrays.toString(Calculator.parseInput(text).toArray()));
 //        String text = "12 65 +597 +1";
